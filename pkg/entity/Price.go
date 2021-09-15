2aff0eac7fc10bc7c9ce002f810b144b3007ae8e
Gus <gustavoocanto@gmail.com>
2021-09-15 17:24:04 +0800

    allow for prices creation + tests

--- pkg/entity/Price.go
+++ pkg/entity/Price.go
@@ -1,16 +1,68 @@
 package entity
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
 type Price struct {
 	Amount int64 `json:"amount"`
-	Currency Currency
+	Currency Currency `json:"currency"`
 }
 
-func (price Price) ToFloat() float64  {
-	//currency := price.Currency
+func (current Price) ToFloat() float64  {
+	amount := float64(current.Amount)
+	multiplier := float64(current.Currency.GetMultiplier())
 
-	fmt.Println("---->", price.Amount)
+	return amount / multiplier
+}
 
-	return float64(price.Amount * 2 / 100)
+func (current Price) ToString() string {
+	return fmt.Sprintf("%s %.2f", current.Currency.Symbol, current.ToFloat())
+}
+
+func MakePrice(currency Currency, abstract interface{}) (Price, error)  {
+	switch value := abstract.(type) {
+		case int:
+			return Price{Amount: int64(value), Currency: currency}, nil
+		case float32:
+			return fromFloat32(currency, value), nil
+		case float64:
+			return fromFloat64(currency, value), nil
+		case string:
+			if price, err := fromString(currency, value); err != nil {
+				return Price{}, err
+			} else {
+				return price, nil
+			}
+		default:
+			return Price{},  errors.New(fmt.Sprintf("The given abstract [%v] is invalid", abstract))
+	}
+}
+
+func fromString(currency Currency, value string) (Price, error) {
+	target, err := strconv.ParseFloat(value, 64)
+
+	if err != nil {
+		return Price{}, err
+	}
+
+	return fromFloat64(currency, target), nil
+}
+
+func fromFloat32(currency Currency, target float32) Price {
+	amount := int64(
+		target * float32(currency.GetMultiplier()),
+	)
+
+	return Price{Amount: amount, Currency: currency}
+}
+
+func fromFloat64(currency Currency, target float64) Price {
+	amount := int64(
+		target * float64(currency.GetMultiplier()),
+	)
+
+	return Price{Amount: amount, Currency: currency}
 }
--- test/unit/entity/price_test.go
+++ test/unit/entity/price_test.go
@@ -1,30 +1,69 @@
 package test_entity
 
 import (
-	"fmt"
+	"github.com/voyago/converter/pkg/entity"
 	"github.com/voyago/converter/test"
 	"testing"
 )
 
-func TestTesting(t *testing.T) {
+func TestItAllowsForBasicFormatting(t *testing.T) {
+	currency := test.Currency(t) //SGD
+
+	price, err := entity.MakePrice(currency, "0.99")
+
+	if err != nil {
+		t.Errorf("%v", err)
+		t.FailNow()
+	}
+
+	if price.ToString() != "$ 0.99" {
+		t.Error("The given price formatting is invalid")
+	}
+}
+
+func TestItAllowsCreationFromIntegers(t *testing.T) {
 	currency := test.Currency(t)
-	//price := entity.Price{Amount: 1099, Currency: currency} //SGD 10.99
-	//
-	//fmt.Println("Result: ", price.ToFloat())
 
-	cost := 1.99 * 100
-	stringValue := "100"
+	price, err := entity.MakePrice(currency, 199)
+	if err != nil || price.Amount != 199 || price.ToFloat() != 1.99 {
+		t.Errorf("The given price [1.99] was not parsed properly")
+	}
 
-	var amount float32 = 57
+	price, err = entity.MakePrice(currency, 1099)
+	if err != nil || price.Amount != 1099 || price.ToFloat() != 10.99 {
+		t.Errorf("The given price [10.99] was not parsed properly")
+	}
+}
 
-	result, _ := fmt.Printf("%.2f\n", amount)
-	fmt.Printf("%T\n", result)
+func TestItAllowsCreationFromStrings(t *testing.T) {
+	currency := test.Currency(t)
 
-	fmt.Println("cost", cost, "---", cost / 100)
-	fmt.Println("string value", stringValue, "---", fmt.Sprint(stringValue))
+	price, err := entity.MakePrice(currency, "0.99")
+	if err != nil || price.Amount != 99 || price.ToFloat() != 0.99 {
+		t.Errorf("The given price [0.99] was not parsed properly")
+	}
 
-	fmt.Println("--------------------------------------------------------------------")
+	price, err = entity.MakePrice(currency, "1.99")
+	if err != nil || price.Amount != 199 || price.ToFloat() != 1.99 {
+		t.Errorf("The given price [1.99] was not parsed properly")
+	}
 
-	//value := 1
-	fmt.Println(currency.GetMultiplier())
+	price, err = entity.MakePrice(currency, "10.99")
+	if err != nil || price.Amount != 1099 || price.ToFloat() != 10.99 {
+		t.Errorf("The given price [10.99] was not parsed properly")
+	}
+}
+
+func TestItAllowsCreationFromFloaters(t *testing.T) {
+	currency := test.Currency(t)
+
+	price, err := entity.MakePrice(currency, 1.99)
+	if err != nil || price.Amount != 199 || price.ToFloat() != 1.99 {
+		t.Errorf("The given price [1.99] was not parsed properly")
+	}
+
+	price, err = entity.MakePrice(currency, 10.99)
+	if err != nil || price.Amount != 1099 || price.ToFloat() != 10.99 {
+		t.Errorf("The given price [10.99] was not parsed properly")
+	}
 }
