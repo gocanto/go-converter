19303da85717ec02ecb333624c9f75edb62c4320
Gus <gustavoocanto@gmail.com>
2021-09-15 15:58:34 +0800

    add multiplier test

--- test/unit/entity/currency_test.go
+++ test/unit/entity/currency_test.go
@@ -1,9 +1,28 @@
 package test_entity
 
 import (
+	"github.com/voyago/converter/pkg/entity"
 	"github.com/voyago/converter/test"
 	"testing"
 )
+
+func TestItComputesValidMultipliers(t *testing.T) {
+	a := entity.Currency{IsoMinorUnit: 1}
+	b := entity.Currency{IsoMinorUnit: 2}
+	c := entity.Currency{IsoMinorUnit: 3}
+
+	if value := a.GetMultiplier(); value != 10 {
+		t.Errorf("The given multiplier [%v] is invalid", value)
+	}
+
+	if value := b.GetMultiplier(); value != 100 {
+		t.Errorf("The given multiplier [%v] is invalid", value)
+	}
+
+	if value := c.GetMultiplier(); value != 1000 {
+		t.Errorf("The given multiplier [%v] is invalid", value)
+	}
+}
 
 func TestItHoldsBasicInfo(t *testing.T) {
 	currency := test.Currency(t)
