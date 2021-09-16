6f4f5c014845e5f7596f0f0d0baaea04bcd9eed6
Gus <gustavoocanto@gmail.com>
2021-09-16 18:24:04 +0800

    remove the store implementation

--- pkg/contracts/currenciesRepository.go
+++
@@ -1,7 +0,0 @@
-package contracts
-
-import "github.com/voyago/converter/pkg/entity"
-
-type CurrenciesRepository interface {
-	FetchCurrency(interface{}) entity.Currency
-}
--- pkg/conversion/converter.go
+++ pkg/conversion/converter.go
@@ -1,27 +1,15 @@
 package conversion
 
 import (
-	"github.com/voyago/converter/pkg/contracts"
 	"github.com/voyago/converter/pkg/entity"
-	store "github.com/voyago/converter/pkg/store/repository"
 )
 
 type Converter struct {
 	Price      entity.Price
-	repository contracts.CurrenciesRepository
 }
 
 func MakeConverter(price entity.Price) Converter {
-	return Converter{Price: price, repository: store.DBRepository{}}
-}
-
-// WithRepository @todo check whether we need this at all
-func (current *Converter) WithRepository(repository contracts.CurrenciesRepository) {
-	(*current).repository = repository
-}
-
-func (current *Converter) WithPrice(price entity.Price) {
-	(*current).Price = price
+	return Converter{Price: price}
 }
 
 func (current Converter) ConvertTo(currency entity.Currency) (entity.Price, error) {
