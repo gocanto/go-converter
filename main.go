b6e21e1373a46f92a222cc2ffb86e5562f1579a0
Gus <gustavoocanto@gmail.com>
2021-09-17 15:43:23 +0800

    work on request

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"io"
+	"net/http"
 )
 
 func main() {
 	myEnv, _ := godotenv.Read()
 
 	fmt.Printf("Welcome to the [%s] library... \n", myEnv["LIBRARY_NAME"])
+
+	resp, _ := http.Get("http://api.currencylayer.com/live?access_key=" + myEnv["CURRENCY_LAYER_KEY"])
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("error closing handler")
+		}
+	}(resp.Body)
+
+	body, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
 }
